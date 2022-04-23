1f54d78bb0507ada086cc0fabd4884b20fe383cf
Puxuan <leslie.qq.lol@gmail.com>
2022-04-23 21:28:05 +0200

    set up pkg

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Interstellarss/faas-share
 go 1.16
 
 require (
+	github.com/openfaas/faas-netes v0.0.0-20220413215428-d20fea667b2d
 	k8s.io/api v0.21.3
 	k8s.io/apimachinery v0.21.3
 )
---
+++ handler_sharepod/deploy.go
@@ -0,0 +1,36 @@
+package handlersharepod
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	k8s "github.com/openfaas/faas-netes/pkg/k8s"
+	"github.com/openfaas/faas-provider/types"
+)
+
+const initialReplicasCount = 1
+
+func MakeDeployHandler(funciotnNamespace string, factory k8s.FunctionFactory) http.HandlerFunc {
+	secret := k8s.NewSecretsClient(factory.Client)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		if r.Body != nil {
+			defer r.Body.Close()
+		}
+
+		body, _ := ioutil.ReadAll(r.Body)
+
+		request := types.FunctionDeployment{}
+		err := json.Unmarshal(body, &request)
+
+		if err != nil {
+			wrappedErr := fmt.Errorf("failed ot unarshal request: %s", err.Error())
+
+		}
+
+	}
+}
