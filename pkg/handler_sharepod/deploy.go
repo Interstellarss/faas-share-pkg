71d8a4c3d089f9c26212a0597719e54094f67c4f
Puxuan <leslie.qq.lol@gmail.com>
2022-04-30 15:53:49 +0200

    make it fit for types.FunctionDeployment

--- pkg/handler_sharepod/deploy.go
+++ pkg/handler_sharepod/deploy.go
@@ -1,6 +1,7 @@
 package handlersharepod
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -13,11 +14,14 @@ import (
 	//"github.com/Interstellarss/faas-share-pkg/pkg/handlersharepod"
 	"github.com/Interstellarss/faas-share-pkg/pkg/sharepod"
 	k8s "github.com/openfaas/faas-netes/pkg/k8s"
+	"github.com/openfaas/faas-provider/types"
 
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 const initialReplicasCount = 1
@@ -34,7 +38,7 @@ func MakeDeployHandler(funciotnNamespace string, factory k8s.FunctionFactory) ht
 
 		body, _ := ioutil.ReadAll(r.Body)
 
-		request := sharepod.SharepodDeployment{}
+		request := types.FunctionDeployment{}
 		err := json.Unmarshal(body, &request)
 
 		if err != nil {
@@ -43,7 +47,7 @@ func MakeDeployHandler(funciotnNamespace string, factory k8s.FunctionFactory) ht
 			return
 		}
 
-		if err := validateDeployRequest(&request); err != nil {
+		if err := ValidateDeployRequest(&request); err != nil {
 			wrappedErr := fmt.Errorf("validation failed: %s", err.Error())
 			http.Error(w, wrappedErr.Error(), http.StatusBadRequest)
 			return
@@ -60,31 +64,62 @@ func MakeDeployHandler(funciotnNamespace string, factory k8s.FunctionFactory) ht
 			return
 		}
 
-		deploymentSpec, err := secrets.GetSecrets(namespace, request.Secrets)
-		if err != nil {
-			wrappedErr := fmt.Errorf("unable to fetch secrets: %s", err, Error())
+		deploymentSpec, specErr := makeDeploymentSpec(request, existingSecrets, factory)
+
+		var profileList []k8s.Profile
+		if request.Annotations != nil {
+			profileNamespace := factory.Config.ProfilesNamespace
+			profileList, err = factory.GetProfiles(ctx, profileNamespace, *request.Annotations)
+			if err != nil {
+				wrappedErr := fmt.Errorf("failed create Deployment spec: %s", err.Error())
+				log.Println(wrappedErr)
+				http.Error(w, wrappedErr.Error(), http.StatusBadRequest)
+				return
+			}
+		}
+
+		for _, profile := range profileList {
+			factory.ApplyProfile(profile, deploymentSpec)
+		}
+
+		if specErr != nil {
+			wrappedErr := fmt.Errorf("failed create Deployment spc: %s", specErr.Error())
+			log.Println(wrappedErr)
 			http.Error(w, wrappedErr.Error(), http.StatusBadRequest)
 			return
 		}
 
-		var profileList []k8s.Profile
-		if request.Annotations != nil {
-			profileNamespace :=
+		deploy := factory.Client.AppsV1().Deployments(namespace)
+
+		_, err = deploy.Create(context.TODO(), deploymentSpec, metav1.CreateOptions{})
+
+		if err != nil {
+			wrappedErr := fmt.Errorf("unable create Deployment: %s", err.Error())
+			log.Println(wrappedErr)
+			http.Error(w, wrappedErr.Error(), http.StatusInternalServerError)
+			return
 		}
 
+		log.Printf("Deployment created: %s.%s\n", request.Service, namespace)
 
+		service := factory.Client.CoreV1().Services(namespace)
+		serviceSpec := makeServiceSpec(request, factory)
+		_, err = service.Create(context.TODO(), serviceSpec, metav1.CreateOptions{})
 
+		if err != nil {
+			wrappedErr := fmt.Errorf("failed create Service: %s", err.Error())
+			log.Println(wrappedErr)
+			http.Error(w, wrappedErr.Error(), http.StatusBadRequest)
+			return
+		}
 
-
-
-
-		log.Printf("")
-		w.WriteHeader(http.StatusAccpeted)
+		log.Printf("Service created: %s.%s\n", request.Service, namespace)
+		w.WriteHeader(http.StatusAccepted)
 
 	}
 }
 
-func makeDeploymentSpec(request sharepod.SharepodDeployment, existingSecrets map[string]*apiv1.Secret, factory k8s.FunctionFactory) (*appsv1.Deployment, error) {
+func makeDeploymentSpec(request types.FunctionDeployment, existingSecrets map[string]*apiv1.Secret, factory k8s.FunctionFactory) (*appsv1.Deployment, error) {
 	envVars := buildEnvVars(&request)
 
 	initialReplicas := int32p(initialReplicasCount)
@@ -111,7 +146,7 @@ func makeDeploymentSpec(request sharepod.SharepodDeployment, existingSecrets map
 	}
 
 	var imagePullPolicy apiv1.PullPolicy
-	switch factory.Config.ImagePullPolicy{
+	switch factory.Config.ImagePullPolicy {
 	case "Never":
 		imagePullPolicy = apiv1.PullNever
 	case "IfNotPresent":
@@ -121,10 +156,143 @@ func makeDeploymentSpec(request sharepod.SharepodDeployment, existingSecrets map
 	}
 
 	//todo: here continue
+	annotations := buildAnnotations(request)
 
+	probes, err := factory.MakeProbes(request)
+
+	if err != nil {
+		return nil, err
+	}
+
+	enableServiceLinks := false
+	allowPrivilegeEscalation := false
+
+	deploymentSpec := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        request.Service,
+			Annotations: annotations,
+			Labels: map[string]string{
+				"faas-share_function": request.Service,
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"faas-share_function": request.Service,
+				},
+			},
+			Replicas: initialReplicas,
+			Strategy: appsv1.DeploymentStrategy{
+				Type: appsv1.RollingUpdateDeploymentStrategyType,
+				RollingUpdate: &appsv1.RollingUpdateDeployment{
+					MaxUnavailable: &intstr.IntOrString{
+						Type:   intstr.Int,
+						IntVal: int32(0),
+					},
+					MaxSurge: &intstr.IntOrString{
+						Type:   intstr.Int,
+						IntVal: int32(1),
+					},
+				},
+			},
+			RevisionHistoryLimit: int32p(10),
+			//TODO: does here need to modify for kubeshare
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        request.Service,
+					Labels:      labels,
+					Annotations: annotations,
+				},
+				Spec: apiv1.PodSpec{
+					//we may not need this part
+					NodeSelector: nodeselector,
+					Containers: []apiv1.Container{
+						{
+							Name:  request.Service,
+							Image: request.Image,
+							Ports: []apiv1.ContainerPort{
+								{
+									Name:          "http",
+									ContainerPort: factory.Config.RuntimeHTTPPort,
+									Protocol:      corev1.ProtocolTCPapiv1.ProtocolTCP,
+								},
+							},
+							Env:             envVars,
+							Resources:       *resources,
+							ImagePullPolicy: imagePullPolicy,
+							LivenessProbe:   probes.Liveness,
+							ReadinessProbe:  probes.Liveness,
+							SecurityContext: &corev1.SecurityContext{
+								ReadOnlyRootFilesystem:   &reques.ReadOnlyRootFilesystem,
+								AllowPrivilegeEscalation: &allowPrivilegeEscalation,
+							},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyAlways,
+					DNSPolicy:     corev1.DNSClusterFirst,
+
+					EnableServiceLinks: &enableServiceLinks,
+				},
+			},
+		},
+	}
+
+	factory.ConfigureReadOnlyRootFilesystem(request, deploymentSpec)
+	factory.ConfigureContainerUserID(deploymentSpec)
+
+	if err := factory.ConfigureSecrets(request, deploymentSpec, existingSecrets); err != nil {
+		return nil, err
+	}
+
+	return deploymentSpec, nil
 }
 
-func buildEnvVars(request *sharepod.SharepodDeployment) []corev1.EnvVar {
+func makeServiceSpec(request types.FunctionDeployment, factory k8s.FunctionFactory) *corev1.Service {
+	serviceSpec := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Sharepod",
+			APIVersion: "kubeshare.nthu/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        request.Service,
+			Annotations: buildAnnotations(request),
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeClusterIP,
+			Selector: map[string]string{
+				"faas-share_function": request.Service,
+			},
+			Ports: []corev1.ServicePort{
+				{
+					Name:     "http",
+					Protocol: corev1.ProtocolTCP,
+					Port:     factory.Config.RuntimeHTTPPort,
+					TargetPort: intstr.IntOrString{
+						Type:   intstr.Int,
+						IntVal: factory.Config.RuntimeHTTPPort,
+					},
+				},
+			},
+		},
+	}
+	return serviceSpec
+}
+
+func buildAnnotations(request types.FunctionDeployment) map[string]string {
+	var annotations map[string]string
+	if request.Annotations != nil {
+		annotations = *request.Annotations
+	} else {
+		annotations = map[string]string{}
+	}
+
+	if _, ok := annotations["prometheus.io.scrape"]; !ok {
+		annotations["prometheus.io.scrapte"] = "false"
+	}
+	return annotations
+}
+
+func buildEnvVars(request *types.FunctionDeployment) []corev1.EnvVar {
 	envVars := []corev1.EnvVar{}
 
 	if len(request.EnvProcess) > 0 {
@@ -147,10 +315,8 @@ func buildEnvVars(request *sharepod.SharepodDeployment) []corev1.EnvVar {
 	return envVars
 }
 
-
-
 func createSelector(constraints []string) map[string]string {
-	selector :=make(map[string]string)
+	selector := make(map[string]string)
 
 	if len(constraints) > 0 {
 		for _, constraint := range constraints {
@@ -160,11 +326,12 @@ func createSelector(constraints []string) map[string]string {
 				selector[parts[0]] = parts[1]
 			}
 		}
-	} 
+	}
 
 	return selector
 }
 
+//TODO: make it fit for types.FunctionDeployment
 func CreateResources(request sharepod.SharepodDeployment) (*sharepod.SharepodRequirements, error) {
 	//need to modify here
 	/*
--- pkg/handler_sharepod/validate.go
+++ pkg/handler_sharepod/validate.go
@@ -5,11 +5,22 @@ import (
 	"regexp"
 
 	"github.com/Interstellarss/faas-share-pkg/pkg/sharepod"
+	"github.com/openfaas/faas-provider/types"
 )
 
 var validDNS = regexp.MustCompile(`^[a-z0-9]([-a-z0-9]*[a-z0-9])?$`)
 
 func validateDeployRequest(request *sharepod.SharepodDeployment) error {
+	//ToDO: service may need to cahnge here?
+	matched := validDNS.MatchString(request.Service)
+	if matched {
+		return nil
+	}
+
+	return fmt.Errorf("(%s) must be a valid DNS entry for service name", request.Service)
+}
+
+func ValidateDeployRequest(request *types.FunctionDeployment) error {
 	//ToDO: service may need to cahnge here?
 	matched := validDNS.MatchString(request.Service)
 	if matched {
